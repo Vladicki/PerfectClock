ceb336dccf11f08b6ec68c4e37090b411b5fe3be
Vladislav Iurev <vlad.yurev.06@gmail.com>
2025-11-08 22:53:31 +0000

    clickable button

--- app/src/main/java/com/griffith/perfectclock/MainActivity.kt
+++ app/src/main/java/com/griffith/perfectclock/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.griffith.perfectclock
 
+import android.R.attr.text
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,18 +9,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,12 +45,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PerfectClockTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                var count by remember{
+                    mutableStateOf(0)
                 }
+
+                    Column (modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Text(text = count.toString(),
+                            fontSize= 20.sp,
+                            color = Color.White
+                            
+                        )
+                        Button(onClick = {count++}) {Text(text="Click me") }
+                    }
+
             }
 
 //            val context = LocalContext.current;
@@ -54,21 +72,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier, ) {
-    Image(painter = painterResource(R.drawable.ic_launcher_foreground), contentDescription = null,
-    modifier = Modifier.background(Color.Red)
-    )
-    LazyColumn {
-    items(10){ i ->
-        for (i in 1..1){
-    Icon( imageVector = Icons.Default.Done , contentDescription = null, Modifier.background(Color.Green))
-}}}}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier, ) {
+//    Image(painter = painterResource(R.drawable.ic_launcher_foreground), contentDescription = null,
+//    modifier = Modifier.background(Color.Red)
+//    )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PerfectClockTheme {
-        Greeting("Vla")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    PerfectClockTheme {
+//        Greeting("Vla")
+//    }
+//}
