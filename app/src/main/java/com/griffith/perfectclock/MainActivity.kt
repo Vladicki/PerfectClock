31f041ac49aa3a1dd0ee6aecc8ecbea66c3ee70f
Vladislav Iurev <vlad.yurev.06@gmail.com>
2025-11-08 23:38:45 +0000

    Simple String manager

--- app/src/main/java/com/griffith/perfectclock/MainActivity.kt
+++ app/src/main/java/com/griffith/perfectclock/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.griffith.perfectclock
 
+import android.R.attr.name
 import android.R.attr.text
+import android.R.attr.top
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,15 +14,22 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -45,33 +54,72 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PerfectClockTheme {
-                var count by remember{
-                    mutableStateOf(0)
-                }
-
-                    Column (modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
+                var name by remember { mutableStateOf("") }
+                var names by remember { mutableStateOf(listOf<String>()) }
+                Column (
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp, top = 50.dp, end = 16.dp)
+                ){
+                    Row(
                     ){
-                        Text(text = count.toString(),
-                            fontSize= 20.sp,
-                            color = Color.White
-                            
+                        OutlinedTextField(
+                            modifier = Modifier.weight(1f),
+                            value = name,
+                            onValueChange = {text ->
+                                name = text
+                            }
                         )
-                        Button(onClick = {count++}) {Text(text="Click me") }
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
+                            if (name.isNotBlank()){
+                            names = names + name
+                            name = ""
+                            }
+                        })
+                        {
+                            Text(text ="Add")
+                        }
                     }
-
+                    LazyColumn {
+                        items(names) { currentName ->
+                            Text(text = currentName,
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .fillMaxWidth(),
+                            )
+                            Divider()
+                        }
+                    }
+                }
             }
+        }
+    }
+}
+
+            //     var count by remember{
+            //         mutableStateOf(0)
+            //     }
+            //
+            //         Column (modifier = Modifier.fillMaxSize(),
+            //             verticalArrangement = Arrangement.Center,
+            //             horizontalAlignment = Alignment.CenterHorizontally
+            //         ){
+            //             Text(text = count.toString(),
+            //                 fontSize= 20.sp,
+            //                 color = Color.White
+            //
+            //             )
+            //             Button(onClick = {count++}) {Text(text="Click me") }
+            //         }
+            //
+            // }
 
 //            val context = LocalContext.current;
 //            Button(onClick = {
 //                intent = Intent(context, Menu::class.java)
 //                startActivity(intent)
 //            }) { }
-        }
-    }
-}
-
 //@Composable
 //fun Greeting(name: String, modifier: Modifier = Modifier, ) {
 //    Image(painter = painterResource(R.drawable.ic_launcher_foreground), contentDescription = null,
